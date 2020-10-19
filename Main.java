999bb74a00cb9189220eee3e17a840039628834e
anthonytissue <anthony.tissue@patelhs.org>
2020-10-19 15:34:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+   int myAge = 15;
+   int myBirthday = 27;
+   int todaysDate = 16;
+   if(myBirthday == todaysDate)
+   {
+     System.out.println("I am" + myAge + "years old.");
+   }
+   else 
+   {
+   System.out.print("I am still " + myAge + " years old.");
+   }
+ 
+  }
+}
